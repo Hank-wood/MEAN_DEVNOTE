e33d9d87078dfcc6ea5980a12071d1ff6b6d59e1
strongwray <953064942@qq.com>
2016-01-02 14:30:44 +0800

    在客户端添加了一些接收功能。

--- example1-socketIO/public/js/client.js
+++ example1-socketIO/public/js/client.js
@@ -1,15 +1,47 @@
-var socket = io();
-  $('form').submit(function(){
-    socket.emit('chatMessage',$('#m').val());
-    $('#m').val('');
-    return false;
-  });
 
-  socket.on('chatMessage', function(msg){
-    $('#messages').append($('<li>').text(msg.userName + ': ' + msg.body));
-  });
+var Chat = function(socket){
+    this.socket = socket;
+}
+//用户发送消息
+Chat.prototype.sendMessage = function(room,text){
+    var message = {
+      room:room,
+      text:text
+    }
+    this.socket.emit('message',message)
+};
+//用户改变房间
+Chat.prototype.changeRoom = function(room){
+    this.socket.emit('join',{
+        newRoom:room
+    })
+};
 
-  socket.on('bye', function(userName) {
-  $('#messages').append($('<li>').text('用户' + userName + ' 离开!'));
-  });
+Chat.prototype.procssCommand = function(commond){
+	var words = command.split(' ');
+	var command = words[0].substring(1,words[0].length).toLowerCase();
+	var message = false;
+	switch(command){
+		case 'join':
+			words.shift(); //删除并返回第一个元素
+			var room = words.join(' ');
+			this.changeRoom(room);
+			break;
+		case 'nick':
+			words.shift();
+			var name = words.join(' ');
+			this.socket.emit('nameAttempt',name)
+			break;
+		default:
+			message = '无法识别的命令'
+			break;
+	}
+	return message;
+}
+
+
+
+
+
+
 
