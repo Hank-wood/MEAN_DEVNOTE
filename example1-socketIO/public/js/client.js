79aead06e503769fde4887f76d83b249ac56263c
strongwray <953064942@qq.com>
2016-01-21 23:57:35 +0800

    重新来维护这个项目，然后重新自己来写聊天室。

--- example1-socketIO/public/js/client.js
+++ example1-socketIO/public/js/client.js
@@ -1,47 +1,7 @@
-
-var Chat = function(socket){
-    this.socket = socket;
-}
-//用户发送消息
-Chat.prototype.sendMessage = function(room,text){
-    var message = {
-      room:room,
-      text:text
-    }
-    this.socket.emit('message',message)
-};
-//用户改变房间
-Chat.prototype.changeRoom = function(room){
-    this.socket.emit('join',{
-        newRoom:room
-    })
-};
-
-Chat.prototype.procssCommand = function(commond){
-	var words = command.split(' ');
-	var command = words[0].substring(1,words[0].length).toLowerCase();
-	var message = false;
-	switch(command){
-		case 'join':
-			words.shift(); //删除并返回第一个元素
-			var room = words.join(' ');
-			this.changeRoom(room);
-			break;
-		case 'nick':
-			words.shift();
-			var name = words.join(' ');
-			this.socket.emit('nameAttempt',name)
-			break;
-		default:
-			message = '无法识别的命令'
-			break;
-	}
-	return message;
-}
-
-
-
-
-
-
-
+$(function(){
+	var socket = io.connect();
+		 socket.on('hello', function(msg){
+		 		console.log(msg.data)
+		 		socket.emit('message',{ data:'来啦',id:msg.id});
+		 });
+})
