ed680fe93d13a81ad99a2118cfb86b94029e8252
strongwray <953064942@qq.com>
2016-01-27 15:03:51 +0800

    修改了页面，添加了时间功能，接下来是解决用户名。

--- example1-socketIO/chat_server.js
+++ example1-socketIO/chat_server.js
@@ -2,14 +2,27 @@
 //初始化一些值	
 	var userCount = 1;
 	var nickNames = [];
+
+//创建每一个连接socket
+	function EachSocket(socket,name){
+		this.userName = name;
+		this.id = socket.id;
+	}
+
 exports.startSocketServer = (app) => {
 		var server = require('http').createServer(app);
 		var io = require('socket.io')(server);
 
 		io.on('connection',(socket) => {
+
 			socket.on('sendMessage',(msg) => {
 				io.sockets.emit('sendMessage',msg);
 			})
+
+			socket.on('disconnect',() => {
+				io.sockets.emit('有人离开了聊天室');
+			})
+
 	  });
 
 		server.listen(app.get('port'),() => {
@@ -17,3 +30,5 @@ exports.startSocketServer = (app) => {
 		});
 
 }
+
+
--- example1-socketIO/public/js/client.js
+++ example1-socketIO/public/js/client.js
@@ -1,19 +1,34 @@
 $(function(){
-	var socket = io.connect();
+//拓展js原生函数
+	Date.prototype.getCurrentTime = Date.getCurrentTime||function(){
+		var changeNumber = function(num){
+			return num<10?'0'+num:num;
+		}
+		var currTime = this.getFullYear()+"-"+(this.getMonth()+1)+"-"+this.getDate()
+		+' '+changeNumber(this.getHours())+':'+changeNumber(this.getMinutes());
+		return currTime;
+	}
 
+
+var socket = io.connect();
 		socket.on('connect', function(){
 			 socket.on('sendMessage',function(msg){
 			 	var date = new Date();
-			 	var currTime = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()
-			 	+' '+date.getHours()+':'+date.getMinutes();
-			 		$('.messageList').append($('<li></li>').text(currTime+' '+msg));
+			 	$('.messageList').append($('<li></li>').text(date.getCurrentTime()+' '+msg));
 			})
 		})
-		
+
+		$('input[name="message"]').keydown(function(event){
+			event.stopPropagation();
+			if(event.which==13){
+				$('#sendMsg').trigger('click')
+			}
+		});
+
 		$('#sendMsg').on('click',function(e){
-				e.preventDefault();
 			 	var clientMsg = $('input[name="message"]').val();
 				socket.emit('sendMessage',clientMsg)
+				$('input[name="message"]').val(' ')
 		})
 
 
