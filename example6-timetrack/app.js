56f324b5bfb2358e56151ebc3d5f4a67eb93fe1d
strongwray <953064942@qq.com>
2016-04-09 00:17:39 +0800

    测试mysql

---
+++ example6-timetrack/app.js
@@ -0,0 +1,54 @@
+'use strict';
+const http = require('http'),
+	  work = require('./lib/timetrack'),
+	  mysql = require('mysql'),
+	  db = mysql.createConnection({
+	  	host:'localhost',
+	  	user:'wulei',
+	  	database:'timetrack'
+	  });
+	  
+let server = http.createServer((req, res) => {
+	switch(req.method) {
+		case 'POST':
+			switch(req.url) {
+  				case '/':
+	  				work.add(db,req,res)
+	  			break;
+  				case '/archive':
+	  				work.archive(db,req,res)
+	  			break;
+		  		case '/delete':
+		  			work.delete(db,req,res)
+		  		break;
+  			}
+			break;
+		case 'GET':
+			switch(req.url) {
+  				case '/':
+	  				work.show(db,res)
+	  			break;
+  				case '/archived':
+	  				work.showArchived(db,res)
+	  			break;
+  			}
+			break;
+	}
+});
+
+db.query("CREATE TABLE IF NOT EXISTS work ("
+	  +'id INT(10) NOT NULL AUTO_INCREMENT,'
+	  +'hours DECIMAL(5,2) DEFAULT 0,'
+	  +'archived INT(1) DEFAULT 0,'
+	  +'description LONGTEXT,'
+	  +'PRIMARY KEY(id),',
+		  function(err){
+		  	if(err) throw err;
+		  	console.log('服务器已经启动')
+		  	server.listen(5000,'localhost')
+		  }
+	  )
+
+
+
+
